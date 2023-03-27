caaf3de519b7c6ff930bf68a85a93d1ccae556a9
ktamburi <ktamburi22@epoka.edu.al>
2023-03-28 01:33:46 +0200

    added sum function

--- calculator/calculator.c
+++ calculator/calculator.c
@@ -3,9 +3,9 @@
 #include <math.h>
 #include <string.h>
 
+void sum();
 int main()
 {
-    double number_1,number_2;
     char operation;
 
     printf("1-Sum of two numbers \"+\"");
@@ -17,6 +17,14 @@ int main()
     printf("\nChoose the symbol of the operation you want to do: ");
     operation=getchar();
     int symbol=operation;
+    if (symbol==43){
+        sum();
+    }
     printf("\nThank you for using this calculator!");
     return 0;
 }
+void sum(){
+    double x,y;
+    printf("\nInsert your numbers:");
+    scanf("%lf%lf",&x,&y);
+    printf("\nThe sum of %.1lf + %.1lf is %.1lf\n",x,y,x+y);}
